f047cc5c9f3892fa69efc5f3ccf02b59ea2f48b8
Moonshiness554 <sasha.jaffarove@gmail.com>
2013-08-07 10:15:53 -0700

    Update and rename README.md to MouseControl.ino

---
+++ MouseControl.ino
@@ -0,0 +1,25 @@
+MouseControl
+_____________
+
+void setup()
+{
+  Mouse.begin();
+  delay(1000);
+  
+  
+  Mouse.move(500,500,0);
+  
+  Mouse.click();
+  
+  Mouse.click(MOUSE_RIGHT);
+  
+  Mouse.release();
+}
+void loop()
+{
+  delay(10);
+  if (Mouse.isPressed())
+  {
+    
+  }
+}
